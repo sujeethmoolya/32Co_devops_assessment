6705369512206eba24f17cad5e077deb8b241b80
smart_sujeeth_97 <109187718+sujeethmoolya@users.noreply.github.com>
2025-08-05 00:04:09 +0530

    Update ec2_python_application.sh
    installing flask

--- infra/template/ec2_python_application.sh
+++ infra/template/ec2_python_application.sh
@@ -5,6 +5,7 @@ yes | sudo apt update
 yes | sudo apt install python3 python3-pip
 yes | sudo apt install python3-pymysql
 sudo apt update
+yes | sudo apt install python3-flask
 git clone https://github.com/sujeethmoolya/python-flash-application-with-database.git
 sleep 20
 # shellcheck disable=SC2164
