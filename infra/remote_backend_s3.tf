4010374215e637447ecdb1af95de210088cab6f7
smart_sujeeth_97 <109187718+sujeethmoolya@users.noreply.github.com>
2025-08-04 07:35:51 +0530

    Update remote_backend_s3.tf

--- infra/remote_backend_s3.tf
+++ infra/remote_backend_s3.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "dev-proj-1-remote-state-bucket-8879493216"
+    bucket = "dev-proj-1-remote-state-bucket-301971"
     key    = "devops-project-1/terraform.tfstate"
     region = "eu-central-1"
   }
